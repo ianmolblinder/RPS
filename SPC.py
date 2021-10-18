faca2d1b57b82644327b791da9ca87ffe89c69d9
ianmolblinder <88677308+ianmolblinder@users.noreply.github.com>
2021-10-18 21:50:43 +0530

    Add files via upload

---
+++ SPC.py
@@ -0,0 +1,36 @@
+import random
+
+
+def is_win(user, computer):
+    pass
+
+
+def is_win(computer, user):
+    pass
+
+
+
+def play():
+    user = input(" What is your choice? 'r' for rock, 'p' for paper, 's' for scissors'")
+    computer = random.choice(['r','p','s'])
+
+    if user == computer:
+        return 'Its a tie'
+
+    # r>s, s>p, p>r
+    def  is_win(user, computer):
+        return 'You Won!'
+
+    def is_win(computer,user):
+        return'You Lost!'
+
+    def is_win(player, opponent):
+        # return true if player wins
+        # r>s, s>p, p>r
+
+        if (player =='r' and opponent =='s') or (player =='s' and opponent =='p') \
+            or (player =='p' and opponent =='r'):
+            return True
+
+print(play())
+
